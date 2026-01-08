31a9d8a5e4dad73026cd4cac492d3e52cd634daa
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2026-01-08 14:58:54 +0700

    fix: Add image domains to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
