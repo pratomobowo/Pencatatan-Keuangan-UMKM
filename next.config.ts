32d49442d014f99b309dd41aa1913d1ea14b9365
Pratomo Bowo Leksono <pratomobowo@gmail.com>
2026-01-12 06:19:41 +0700

    security: implement robust HTTP security headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
     reactStrictMode: true,
     images: {
         remotePatterns: [
-
             {
                 protocol: 'https',
                 hostname: 'lh3.googleusercontent.com',
@@ -18,6 +17,50 @@ const nextConfig: NextConfig = {
                 hostname: 'images.pexels.com',
             },
         ],
+    },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: [
+                            "default-src 'self'",
+                            "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+                            "style-src 'self' 'unsafe-inline' fonts.googleapis.com",
+                            "img-src 'self' blob: data: lh3.googleusercontent.com images.unsplash.com images.pexels.com",
+                            "font-src 'self' fonts.gstatic.com",
+                            "connect-src 'self' ark.ap-southeast.bytepluses.com",
+                            "frame-src 'self'",
+                            "object-src 'none'",
+                            "base-uri 'self'",
+                            "form-action 'self'",
+                        ].join('; '),
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+                    },
+                ],
+            },
+        ];
     },
 };
 
